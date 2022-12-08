3ca4a5d23a777564f7c0c2ad819fdffc175c47c8
Fitch-Gamer <83094901+Fitch-Gamer@users.noreply.github.com>
2022-12-08 09:19:07 +0000

    Revert "Backup"

--- 2d dungeon/Assets/Scripts/Map/MapSetup.cs
+++ 2d dungeon/Assets/Scripts/Map/MapSetup.cs
@@ -1,126 +1,44 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Main.RankStructure;
 
 namespace Main
 {
-
-    public class Node
-    {
-        public float x, y;
-        public GameObject MapObj;
-        public bool Entity;
-
-        public Vector2 GetActualLoc()
-        {
-            return new Vector2((0.16f * x) + 0.08f, (0.16f * y) + 0.08f);
-        }
-        public Node(float Xloc, float Yloc, GameObject Obj, bool entity)
-        {
-            x = Xloc;
-            y = Yloc;
-            MapObj = Obj;
-            Entity = entity;
-        }
-        public Vector2 ToNodeLoc(Vector2 ActualLoc)
-        {
-            return new Vector2(Mathf.Round((ActualLoc.x - 0.08f) / 0.16f), Mathf.Round((ActualLoc.y - 0.08f) / 0.16f));
-        }
-    }
     public class MapSetup : MonoBehaviour
     {
         public int[,] Map = new int[1024, 1024];
-        public Node[,] NodeMap = new Node[1024, 1024];
+
         public Vector2 SpawnLoc = Vector2.zero;
-        public GameObject Floor;
+        public GameObject floor;
         public GameObject Block;
         public GameObject SpawnPoint;
-        public GameObject Player;
-        public GameObject follower;
-        public GameObject watcher;
-        public GameObject AI;
-        //int squadnum = 0;
-        static public AIPlatoon platoon = new AIPlatoon("0", new List<AISquad>());
-        static public AISquad squad = new AISquad("1", new List<AIModel>());
-        //public GameObject Enemy;
-        //public GameObject Door;
+        public GameObject Enemy;
+        public GameObject Door;
         // Start is called before the first frame update
         void Start()
         {
             Map = publicvariables.Map;
-            Map[0, 0] = 2;
-            Map[1, 0] = 1;
-            Map[2, 0] = 3;
-            Map[3, 0] = 1;
-            Map[4, 0] = 1;
-            Map[5, 0] = 1;
-            Map[6, 0] = 4;
-            Map[7, 0] = 5;
-            platoon.Squads.Add(squad);
-            
             for (int x = 0; x < Map.GetLength(0); x++)
             {
                 for (int y = 0; y < Map.GetLength(1); y++)
                 {
+                    GameObject piece = new GameObject();
                     switch (Map[x, y])
                     {
+                        case 0:
+                            piece = floor;
+                            break;
                         case 1:
-                            
-                            
-                            NodeMap[x, y] = new Node(x, y,Instantiate(Floor),false);
-                            NodeMap[x, y].MapObj.transform.position = new Vector2((float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NodeMap[x, y].MapObj.GetComponent<SpriteRenderer>().enabled = true;
+                            piece = Block;
                             break;
                         case 2:
-                            NodeMap[x, y] = new Node(x, y, Instantiate(Block),false);
-                            NodeMap[x, y].MapObj.transform.position = new Vector2((float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NodeMap[x, y].MapObj.GetComponent<SpriteRenderer>().enabled = true;
+                            piece = SpawnPoint;
+                            SpawnLoc = new Vector2((0.16f * x) + 0.08f, (0.16f * y) + 0.08f);
                             break;
-                        case 3:
-                            NodeMap[x, y] = new Node(x, y, Instantiate(SpawnPoint),false);
-                            NodeMap[x, y].MapObj.transform.position = new Vector2((float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NodeMap[x, y].MapObj.GetComponent<SpriteRenderer>().enabled = true;
-                            SpawnLoc = new Vector2((16f * x) + 8f, (16f * y) + 8f);
-                            break;
-                        case 4:
-                            NodeMap[x, y] = new Node(x, y, Instantiate(Floor),true);
-                            NodeMap[x, y].MapObj.transform.position = new Vector2((float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NodeMap[x, y].MapObj.GetComponent<SpriteRenderer>().enabled = true;
-                            GameObject NewAI = Instantiate(AI);
-                            AIModel SQModel = new AIModel("AI" + x + y, 100, 30, 120, false, false, RoleType.SQUADDIE);
-                            NewAI.transform.position = new Vector2((float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NewAI.GetComponent<SpriteRenderer>().enabled = true;
-                            NewAI.GetComponent<AIDecision>().enabled = true;
-                            NewAI.GetComponent<AIDecision>().AI = SQModel;
-                            //platoon.Squads[platoon.Squads.Count - 1].Models.Add(SQModel);      an attempt to create a new squad for each commander however this causes issues with order of initialisation of commanders and squaddies
-                            squad.Models.Add(NewAI.GetComponent<AIDecision>().AI);
-                            Main.publicvariables.AllEnemy.Add(NewAI);
-                            break;
-                        case 5:
-                            NodeMap[x, y] = new Node(x, y, Instantiate(Floor), true);
-                            NodeMap[x, y].MapObj.transform.position = new Vector2((float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NodeMap[x, y].MapObj.GetComponent<SpriteRenderer>().enabled = true;
-                            GameObject NewAIC = Instantiate(AI);
-                            AIModel AICModel= new AIModel("AI" + x + y, 100, 30, 120, true, false, RoleType.COMMANDER);
-                            NewAIC.transform.position = new Vector2( (float)Math.Round((16f * x) + 8f,2), (16f * y) + 8f);
-                            NewAIC.GetComponent<SpriteRenderer>().enabled = true;
-                            NewAIC.GetComponent<AIDecision>().enabled = true;
-                            NewAIC.GetComponent<AIDecision>().AI = AICModel;
-                            squad.Models.Add(NewAIC.GetComponent<AIDecision>().AI);
-                            //AISquad NewSquad = new AISquad(squadnum.ToString(), new List<AIModel>);   lookabove, continued attempt to make a new squad
-                            //NewSquad.Models.Add(AICModel);
-                            //platoon.Squads.Add(NewSquad);
-                            Main.publicvariables.AllEnemy.Add(NewAIC);
-                            break;
-
                     }
+                    piece.transform.position = new Vector2((0.16f * x) + 0.08f, (0.16f * y) + 0.08f);
                 }
             }
-            Player.transform.position = SpawnLoc;
-            follower.transform.position = SpawnLoc;
-            watcher.transform.position = new Vector3(SpawnLoc.x, SpawnLoc.y, -10) ;
         }
 
         // Update is called once per frame
--- 2d dungeon/Assets/Scripts/Player/PlayerController.cs
+++ 2d dungeon/Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Main.RankStructure;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,20 +9,11 @@ public class PlayerController : MonoBehaviour
     public float dist;
     public float constz;
     public LayerMask colliders;
-    public float Health;
-    public int WeaponAmmo;
-    public int WeaponDamage;
-    public int ReserveAmmo;
-    public Rigidbody2D Bullet;
-    public float ShotSpeed;
-    public Camera Cam;
     // Start is called before the first frame update
     void Start()
     {
-        Health = 100;
         movepoint.parent = null;
         constz = transform.position.z;
-        colliders = LayerMask.GetMask("MapObjects", "EnemyAI");
     }
 
     // Update is called once per frame
@@ -33,77 +22,25 @@ public class PlayerController : MonoBehaviour
         transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, constz), new Vector3(movepoint.position.x,movepoint.position.y, constz), movespeed * Time.deltaTime);
         if (Vector3.Distance(transform.position, movepoint.position) == 0 && Main.publicvariables.playerattacking == false)
         {
-            
             Main.publicvariables.playermoving = false;
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movepoint.position + new Vector3(Input.GetAxisRaw("Horizontal") * dist, 0f, 0f), (float)Math.Round((dist/2f),2),colliders)) //TODO Fix 2.3f divider, should be 2f
+                if (!Physics2D.OverlapBox(movepoint.position + new Vector3(Input.GetAxisRaw("Horizontal")*dist,0f,0f), new Vector2(dist,dist),0))
                 {
                     Main.publicvariables.playermoving = true;
                     movepoint.position += new Vector3(Input.GetAxisRaw("Horizontal") * dist, 0f, 0f);
                     Main.publicvariables.playerdirection = 2*Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
-                    BroadcastToEnemy("Action");
                 }
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movepoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical") * dist, 0f), (float)Math.Round((dist/2f),2),colliders))//TODO Fix 2.3f divider, should be 2f
+                if (!Physics2D.OverlapBox(movepoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical") * dist,  0f), new Vector2(dist, dist), 0))
                 {
                     Main.publicvariables.playermoving = true;
                     movepoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") * dist, 0f);
                     Main.publicvariables.playerdirection = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
-                    BroadcastToEnemy("Action");
                 }
             }
-            if(Input.GetMouseButtonDown(0))
-            {
-                float RandAngle = UnityEngine.Random.Range(-5f, 5f);
-                Rigidbody2D Shot = Instantiate(Bullet);
-                Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), Shot.GetComponent<Collider2D>(),true);
-                Shot.position = this.transform.position;
-                Shot.gameObject.transform.Rotate(new Vector3(0, 0, LinearAngle(Cam.ScreenToWorldPoint(Input.mousePosition), transform.position) + RandAngle));
-                Shot.GetComponent<SpriteRenderer>().enabled = true;
-                Shot.velocity = new Vector2((Cam.ScreenToWorldPoint(Input.mousePosition).x - this.transform.position.x), (Cam.ScreenToWorldPoint(Input.mousePosition).y - this.transform.position.y)).normalized * ShotSpeed;
-            }
         }
-    }
-    public void BroadcastToEnemy(string ToBroadcast)
-    {
-        foreach(GameObject g in Main.publicvariables.AllEnemy)
-        {
-            g.SendMessage(ToBroadcast);
-        }
-    }
-    public void TakeDmg(int dmg)
-    {
-        Health -= dmg;
-    }
-    private float LinearDistance(Vector2 position1, Vector2 position2)
-    {
-        return (float)(Math.Sqrt(Math.Pow((position1.x - position2.x), 2) + Math.Pow(position1.y - position2.y, 2)));
-    }
-
-    private float LinearAngle(Vector2 position1, Vector2 position2)
-    {
-        float pos1x = position1.x;
-        float pos1y = position1.y;
-        float pos2x = position2.x;
-        float pos2y = position2.y;
-        float ans = 0;
-        if (pos2x > pos1x && pos2y > pos1y)
-        {
-            ans = Mathf.Atan((pos2y - pos1y) / (pos2x - pos1x)) * 180 / Mathf.PI;
-            //ans += 180;
-        }
-        else
-        {
-            ans = Mathf.Atan((pos1y - pos2y) / (pos1x - pos2x)) * 180 / Mathf.PI;
-        }
-        if (ans < 0)
-        {
-            ans += 360;
-        }
-
-        return ans;
     }
 }
